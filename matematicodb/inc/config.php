583f8a1bb5be7a3a75e88ef0d08acb235bdb4714
Matej Maca <macam@arcig.cz>
2024-11-10 09:45:02 +0100

    modified db credentials

--- matematicodb/inc/config.php
+++ matematicodb/inc/config.php
@@ -1,5 +1,5 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "matematico_db");
+define("DB_HOST", "sql303.infinityfree.com");
+define("DB_USERNAME", "if0_37622898");
+define("DB_PASSWORD", "PY2q0CjhVD");
+define("DB_NAME", "if0_37622898_matematico_db");
