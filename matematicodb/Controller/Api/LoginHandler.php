dc27912320612cf8049aa053935b477f57ad151f
Matej Maca <macam@arcig.cz>
2024-11-11 21:14:17 +0100

    actually send it

--- matematicodb/Controller/Api/LoginHandler.php
+++ matematicodb/Controller/Api/LoginHandler.php
@@ -7,10 +7,10 @@ class LoginHandler {
         $username = $query["username"];
         $password = $query["password"];
         header("Content-Type: application/json");
-        $db_query = $database->select("SELECT * FROM `users` WHERE `username`='" . $username . "' && `password`='" . $password . "'");
+        $db_query = $database->select("SELECT `high-score` FROM `users` WHERE `username`='" . $username . "' && `password`='" . $password . "'");
         if($db_query) {
             header("HTTP/1.1 200 OK");
-            echo json_encode("high-score", $db_query["high-score"]);
+            echo json_encode($db_query);
         } else {
             header("HTTP/1.1 300 Failed");
             echo json_encode("fail");
