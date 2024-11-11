facea156699bde4c56d9098b9071a083595ff552
Matej Maca <macam@arcig.cz>
2024-11-11 18:40:21 +0100

    another test

--- login/login-script.js
+++ login/login-script.js
@@ -4,6 +4,7 @@ var response;
 
 function loginBtClick() {
     //funny comment just for testing
+    alert(test);
     getValues();
     fetch(`http://matematico.great-site.net/matematicodb/index.php/login?username=${username_input}&password=${password_input}`, {
         headers: {
