5fbd844211b62765021527a26ffc91140d3e24dc
Matej Maca <macam@arcig.cz>
2024-11-11 20:27:35 +0100

    db control changes

--- matematicodb/index.php
+++ matematicodb/index.php
@@ -1,10 +1,8 @@
-ahoj
 <?php
 require __DIR__ . "/inc/bootstrap.php";
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
-header('Access-Control-Allow-Origin: *');
 
 require ROOT_PATH . "/Controller/RouteHandler.php";
 $route_handler = new RouteHandler();
