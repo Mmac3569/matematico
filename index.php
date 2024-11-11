ecd3e62553dbdcbed49d4652234f10bf6582fa97
Matej Maca <macam@arcig.cz>
2024-11-11 19:44:10 +0100

    better cache buster

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Matematico online</title>
     <link rel="stylesheet" href="styles.css">
-    <script src="http://matematico.great-site.net/script.js?t=<?php echo time() ?>"></script>
-    <script src="http://matematico.great-site.net/counting-script.js?t=<?php echo time() ?>"></script>
+    <script src="http://matematico.great-site.net/script.js?v=<?php echo filemtime("login-script.js") ?>"></script>
+    <script src="http://matematico.great-site.net/counting-script.js?v=<?php echo filemtime("login-script.js") ?>"></script>
 </head>
 <body onload="init()">
     <header>
