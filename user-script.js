9c587d775bb66e5b57a4558da881a260bd0f87c8
Matej Maca <macam@arcig.cz>
2024-11-12 18:07:32 +0100

    show high score on display

--- user-script.js
+++ user-script.js
@@ -9,7 +9,7 @@ class user {
         let response = await fetch("http://matematico.great-site.net/matematicodb/index.php/high-score?id=" + user_id);
         let json = await response.json();
         if (response.ok) {
-            console.log(json[0]);
+            console.log(json[0]["high-score"]);
             high_score_display = json[0]["high-score"];
         }
     }
