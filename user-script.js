be55e62554720a5b3ffe782e174d495a328f5360
Matej Maca <macam@arcig.cz>
2024-11-12 18:38:02 +0100

    added a way to set high score

--- user-script.js
+++ user-script.js
@@ -2,7 +2,7 @@
 async function displayHighScore() {
     let query_string = window.location.search;
     let user_id = new URLSearchParams(query_string).get("id");
-    let response = await fetch("http://matematico.great-site.net/matematicodb/index.php/high-score?id=" + user_id);
+    let response = await fetch("http://matematico.great-site.net/matematicodb/index.php/high-score/get?id=" + user_id);
     let json = await response.json();
     if (response.ok) {
         console.log(json[0]["high-score"]);
@@ -13,5 +13,5 @@ async function displayHighScore() {
 async function setHighScore(value) {
     let query_string = window.location.search;
     let user_id = new URLSearchParams(query_string).get("id");
-    await fetch("http://matematico.great-site.net/matematicodb/index.php/high-score?id=" + user_id + "&value=" + value);
+    await fetch("http://matematico.great-site.net/matematicodb/index.php/high-score/set?id=" + user_id + "&value=" + value);
 }
