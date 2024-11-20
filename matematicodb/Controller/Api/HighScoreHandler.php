48709f1180ce07773d75e106a618f352c4c30d44
Matej Maca <macam@arcig.cz>
2024-11-20 20:43:40 +0100

    backend db changes

--- matematicodb/Controller/Api/HighScoreHandler.php
+++ matematicodb/Controller/Api/HighScoreHandler.php
@@ -6,10 +6,11 @@ class HighScoreHandler {
         $database = new Database();
         $id = $query["id"];
         header("Content-Type: application/json");
-        $db_query = $database->select("SELECT `high-score` FROM `users` WHERE `session_id`=" . $id);
-        if($db_query) {
+        $db_query = $database->select("SELECT `username` FROM `users` WHERE `session_id`=" . $id);
+        $db_st = $database->select("SELECT `high-score` FROM `scores` WHERE `username`=" . $db_query[0]["username"]);
+        if($db_st) {
             header("HTTP/1.1 200 OK");
-            echo json_encode($db_query);
+            echo json_encode($db_st);
         } else {
             header("HTTP/1.1 300 Failed");
             echo json_encode("fail");
@@ -23,8 +24,9 @@ class HighScoreHandler {
         $id = $query["id"];
         $value = $query["value"];
         header("Content-Type: application/json");
-        $db_query = $database->executeStatement("UPDATE `users` SET `high-score` = '" . $value . "' WHERE `users`.`session_id`=" . $id);
-        if($db_query) {
+        $db_query = $database->select("SELECT `username` FROM `users` WHERE `session_id`=" . $id);
+        $db_st = $database->executeStatement("UPDATE `scores` SET `high-score` = '" . $value . "' WHERE `scores`.`username`=" . $db_query[0]["username"]);
+        if($db_st) {
             header("HTTP/1.1 200 OK");
         } else {
             header("HTTP/1.1 300 Failed");
