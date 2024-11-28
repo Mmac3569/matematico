1cdec433be86bf76e3b3c5a8c40019a8d67dd605
Matej Maca <macam@arcig.cz>
2024-11-28 20:50:55 +0100

    fix join not working right

--- multiplayer/multiplayer-script.js
+++ multiplayer/multiplayer-script.js
@@ -1,13 +1,13 @@
 
 
 async function joinGame() {
-    let code_input = "x" + document.getElementById("code-in").value + "x";
+    let code_input = document.getElementById("code-in").value;
     let user_id = new URLSearchParams(window.location.search).get("id");
     if(user_id == null) {
         alert("Abyste mohli hrát multiplayer musíte se přihlásit nebo zaregistrovat!");
         return -1;
     }
-    response = await fetch(`http://matematico.great-site.net/matematicodb/index.php/multiplayer/join?code=${code_input}&id=${user_id}`);
+    response = await fetch(`http://matematico.great-site.net/matematicodb/index.php/multiplayer/join?code=x${code_input}x&id=${user_id}`);
     if(response.ok) {
         alert("join successful");
     } else {
