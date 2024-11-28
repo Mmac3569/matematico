56e30fc74bfb3d331662d9393af3c5066780db4d
Matej Maca <macam@arcig.cz>
2024-11-28 20:55:16 +0100

    fix script not updating

--- multiplayer/index.php
+++ multiplayer/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="multiplayer-script.js?v=<?php echo filemtime("login-script.js") ?>"></script>
+    <script src="multiplayer-script.js?v=<?php echo filemtime("multiplayer-script.js") ?>"></script>
     <title>Matematico - multiplayer</title>
 </head>
 <body>
