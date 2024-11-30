31e1e4ada5ae8b4bd43eec7c2126ae848a54e9fd
Matej Maca <macam@arcig.cz>
2024-11-30 11:23:09 +0100

    show player id in players and game code

--- multiplayer/multiplayer-script.js
+++ multiplayer/multiplayer-script.js
@@ -14,7 +14,7 @@ async function joinGame() {
         game_code = code_input;
         sse_source = new EventSource("http://matematico.great-site.net/matematicodb/Controller/SSE/GameSSE.php");
         sse_source.onmessage = handleSSE;
-        showParty();
+        showParty(false);
     } else {
         alert("Kód " + code_input + " neexistuje nebo přihlášení není platné");
     }
@@ -32,15 +32,22 @@ async function createNewGame() {
         game_code = await response.text();
         sse_source = new EventSource("http://matematico.great-site.net/matematicodb/Controller/SSE/GameSSE.php");
         sse_source.onmessage = handleSSE;
-        showParty();
+        showParty(true);
     } else {
         alert("Přihlášení není platné");
     }
 }
 
-async function showParty() {
+async function showParty(master) {
     let response = await fetch("http://matematico.great-site.net/multiplayer/party.html?v=" + Date.now());
     if(response.ok) {
         document.body.innerHTML = await response.text();
+        document.getElementById("header").innerHTML += game_code;
+        if(master) {
+            document.getElementsByClassName("game-controls").forEach(element => {
+                element.disabled = false;                
+            });
+        }
+        document.getElementById("players-div").innerHTML += "<h5>" + new URLSearchParams(window.location.search).get("id") + "</h5>";
     }
 }
