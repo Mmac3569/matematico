b7049d40f2318331c077bcde011762a2d0d03e2c
Matej Maca <macam@arcig.cz>
2024-11-30 10:38:46 +0100

    fixed not showing party site

--- multiplayer/multiplayer-script.js
+++ multiplayer/multiplayer-script.js
@@ -41,7 +41,6 @@ async function createNewGame() {
 async function showParty() {
     let response = await fetch("http://matematico.great-site.net/multiplayer/party.html");
     if(response.ok) {
-        console.log(await response.text());
         document.body = await response.text();
     }
 }
