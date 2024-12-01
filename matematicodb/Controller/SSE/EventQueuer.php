4d82f08e1c1eaba87a0c9794a695ac122e400dcf
Matej Maca <macam@arcig.cz>
2024-12-01 15:02:39 +0100

    debugging

--- matematicodb/Controller/SSE/EventQueuer.php
+++ matematicodb/Controller/SSE/EventQueuer.php
@@ -1,12 +1,12 @@
 <?php
+$eventqueue_file_path = ROOT_PATH . "/Controller/SSE/eventqueue.txt";
 class EventQueuer {
-    const eventqueue_file_path = ROOT_PATH . "/Controller/SSE/eventqueue.txt";
 
     function queuePlayerUpdate($game_id, $username, $type) {
-        file_put_contents(eventqueue_file_path, $game_id . "\nPlayerUpdate\n" . $username . "\n" . $type . "\n###\n", FILE_APPEND | LOCK_EX);
+        file_put_contents($eventqueue_file_path, $game_id . "\nPlayerUpdate\n" . $username . "\n" . $type . "\n###\n", FILE_APPEND | LOCK_EX);
     }
     
     function queueStartUpdate($game_id, $speed, $mode) {
-        file_put_contents(eventqueue_file_path, $game_id . "\nStartUpdate\n" . $speed . "\n" . $mode . "\n###\n", FILE_APPEND | LOCK_EX);
+        file_put_contents($eventqueue_file_path, $game_id . "\nStartUpdate\n" . $speed . "\n" . $mode . "\n###\n", FILE_APPEND | LOCK_EX);
     }
 }
