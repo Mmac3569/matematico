a8bfe6eacc02d1659f8cd964ac1331ab2e19106d
Matej Maca <macam@arcig.cz>
2024-12-31 10:26:16 +0100

    bug fix

--- matematicodb/Controller/SSE/GameSSE.php
+++ matematicodb/Controller/SSE/GameSSE.php
@@ -1,26 +1,22 @@
 test<?php
-include_once "../../inc/bootstrap.php"; echo "ok";
+include_once "../../inc/bootstrap.php";
 //header("Content-Type: text/event-stream");
-header('Cache-Control: no-cache'); echo "ok";
+header('Cache-Control: no-cache');
 sendUpdatesFromQueue();
 
 function sendUpdatesFromQueue() {
-    parse_str($_SERVER['QUERY_STRING'], $query); echo "ok";
-    $username = $query["username"]; echo "ok";
-    $file_content = file_get_contents(ROOT_PATH . "/Controller/SSE/eventqueue.txt"); echo "ok";
-    echo $file_content;
-    echo $username;
-    echo strpos($file_content, $username);
-    echo !strpos($file_content, $username);
+    parse_str($_SERVER['QUERY_STRING'], $query);
+    $username = $query["username"];
+    $file_content = file_get_contents(ROOT_PATH . "/Controller/SSE/eventqueue.txt"); 
     while(!strpos($file_content, $username)) {
         sleep(5);
-        $file_content = file_get_contents(ROOT_PATH . "/Controller/SSE/eventqueue.txt"); echo "loop";
+        $file_content = file_get_contents(ROOT_PATH . "/Controller/SSE/eventqueue.txt");
     }
-    $update_part = substr($file_content, strpos($file_content, $username)); echo "ok";
-    $update_data = substr($update_part, 0, strpos($update_part, "###")); echo "ok";
-    echo "data: " . $update_data . PHP_EOL . PHP_EOL; echo "ok";
-    $file_content = str_replace($update_data, '', $file_content); echo "ok";
-    file_put_contents(ROOT_PATH . "/Controller/SSE/eventqueue.txt", $file_content); echo "ok";
+    $update_part = substr($file_content, strpos($file_content, $username)); 
+    $update_data = substr($update_part, 0, strpos($update_part, "###")); 
+    echo "data: " . $update_data . PHP_EOL . PHP_EOL; 
+    $file_content = str_replace($update_data . "###\n", '', $file_content);
+    file_put_contents(ROOT_PATH . "/Controller/SSE/eventqueue.txt", $file_content);
     ob_flush();
     flush();
 }
