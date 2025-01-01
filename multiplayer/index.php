9289e0e2b7f990d6a91254ed467f1b341c0f1e74
Matej Maca <macam@arcig.cz>
2025-01-01 12:47:47 +0100

    added the game script to the page

--- multiplayer/index.php
+++ multiplayer/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="multiplayer-script.js?v=<?php echo filemtime('multiplayer-script.js') ?>"></script>
     <script src="sse-handler-script.js?v=<?php echo filemtime('sse-handler-script.js') ?>"></script>
+    <script src="game-script.js?v=<?php echo filemtime('game-script.js') ?>"></script>
     <link rel="stylesheet" href="styles.css?v=<?php echo filemtime('styles.css') ?>">
     <title>Matematico - multiplayer</title>
 </head>
