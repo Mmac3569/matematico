36c2107c839436053d6e2a7aeed8ca66f4d4207a
Matej Maca <macam@arcig.cz>
2025-01-01 09:38:57 +0100

    added scripts to game page

--- multiplayer/sse-handler-script.js
+++ multiplayer/sse-handler-script.js
@@ -29,10 +29,10 @@ async function handleStart(data) {
     if(response.ok) {
         document.body.innerHTML = await response.text();
         document.getElementById("header").innerHTML += game_code;
-        document.head += "<link rel='stylesheet' href='https://matematico.great-site.net/styles.css?v=" + Date.now() + "'>";
-        document.head += "<script src='https://matematico.great-site.net/script.js?v=" + Date.now() + "'></script>";
-        document.head += "<script src='https://matematico.great-site.net/counting-script.js?v=" + Date.now() + "'></script>";
-        document.head += "<script src='https://matematico.great-site.net/user-script.js?v=" + Date.now() + "'></script>";
+        document.head.innerHTML += "<link rel='stylesheet' href='https://matematico.great-site.net/styles.css?v=" + Date.now() + "'>";
+        document.head.innerHTML += "<script src='https://matematico.great-site.net/script.js?v=" + Date.now() + "'></script>";
+        document.head.innerHTML += "<script src='https://matematico.great-site.net/counting-script.js?v=" + Date.now() + "'></script>";
+        document.head.innerHTML += "<script src='https://matematico.great-site.net/user-script.js?v=" + Date.now() + "'></script>";
         sse_source.close();
     }
 }
