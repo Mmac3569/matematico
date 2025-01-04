a8d4045fd430a7d9be9ea646726ada9e7559a739
Matej Maca <macam@arcig.cz>
2025-01-04 17:41:51 +0100

    bug fix

--- multiplayer/sse-handler-script.js
+++ multiplayer/sse-handler-script.js
@@ -36,19 +36,17 @@ async function handleStart(data) {
         styles.href = 'https://matematico.great-site.net/styles.css?v=' + Date.now();
         document.head.appendChild(styles);
 
-        /* let script1 = document.createElement('script');
-        script1.src = 'https://matematico.great-site.net/script.js?v=' + Date.now();
-        document.head.appendChild(script1); */
-
         let script2 = document.createElement('script');
         script2.src = 'https://matematico.great-site.net/counting-script.js?v=' + Date.now();
         document.head.appendChild(script2);
 
         let script3 = document.createElement('script');
         script3.src = 'https://matematico.great-site.net/user-script.js?v=' + Date.now();
+        script3.onload = function() {
+            init();
+        }
         document.head.appendChild(script3);
 
         sse_source.close();
-        document.body.onload = function() { init(); };
     }
 }
