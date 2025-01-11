fd75567abd68fd27f2e22a399d58a64b266c3f3d
Matej Maca <macam@arcig.cz>
2025-01-11 10:52:53 +0100

    timeout sse error prevention

--- matematicodb/Controller/SSE/GameSSE.php
+++ matematicodb/Controller/SSE/GameSSE.php
@@ -7,10 +7,16 @@ sendUpdatesFromQueue();
 function sendUpdatesFromQueue() {
     parse_str($_SERVER['QUERY_STRING'], $query);
     $username = $query["username"];
-    $file_content = file_get_contents(ROOT_PATH . "/Controller/SSE/eventqueue.txt"); 
+    $file_content = file_get_contents(ROOT_PATH . "/Controller/SSE/eventqueue.txt");
+    $waited = 0; 
     while(!strpos($file_content, $username)) {
         sleep(5);
+        $waited += 5;
         $file_content = file_get_contents(ROOT_PATH . "/Controller/SSE/eventqueue.txt");
+        if ($waited > 60) {
+            header("HTTP/1.1 204 No Updates");
+            exit;
+        }
     }
     $update_part = substr($file_content, strpos($file_content, $username));
     $update_data = substr($update_part, 0, strpos($update_part, "###"));
